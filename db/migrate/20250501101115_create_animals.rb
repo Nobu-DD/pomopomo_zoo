7e7757c7c74c681daaa65a7d686e0e69ec7baec7
nobuhiro-sato <n.s.mh256@gmail.com>
2025-05-01 19:55:47 +0900

    add:Animals_migrate実施

--- db/migrate/20250501101115_create_animals.rb
+++ db/migrate/20250501101115_create_animals.rb
@@ -1,8 +1,8 @@
 class CreateAnimals < ActiveRecord::Migration[7.2]
   def change
     create_table :animals do |t|
-      t.string :image_name
-      t.string :
+      t.string :image_name, null: false
+      t.string :image, null:false
 
       t.timestamps
     end
